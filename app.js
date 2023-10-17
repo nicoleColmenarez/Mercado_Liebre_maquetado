b407237b7fe07fae1e3c5c05e434b36f09a8e0a1
Nicole Colmenarez <nicolecolmenares412@gmail.com>
2023-10-17 03:52:24 -0300

    feat : estructuring

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
